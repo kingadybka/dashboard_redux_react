6560c1a40f78c78255c7489f0bd01069f10ed0e9
Kinga Dybka <fokakissme@gmail.com>
2022-07-13 12:10:05 +0200

    App.js change

--- src/App.js
+++ src/App.js
@@ -1,39 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { Container, Navbar, Spinner } from "react-bootstrap";
-// import UserList from "./features/users/UserList";
-// import { AddUser } from "./features/users/AddUser";
-// import { EditUser } from "./features/users/EditUser";
-// import { Route, Routes, Link } from "react-router-dom";
-// import { useSelector, useDispatch } from "react-redux";
-// import { fetchUsers } from "./store/usersSlice";
-
-// const App = () => {
-//   const dispatch = useDispatch();
-//   const { isLoading } = useSelector(({ users }) => users);
-
-//   useEffect(() => {
-//     dispatch(fetchUsers());
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <Navbar bg="dark" variant="dark">
-//         <Container>
-//           <Navbar.Brand href="/">Dashboard</Navbar.Brand>
-//         </Container>
-//       </Navbar>
-//       <Routes>
-//         <Route exact path="/" element={<UserList />} />
-//         <Route path="add" element={<AddUser />} />
-//         <Route path="edit/:id" element={<EditUser />} />
-//       </Routes>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
 import React, { useEffect } from "react";
 import { Container, Navbar } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
